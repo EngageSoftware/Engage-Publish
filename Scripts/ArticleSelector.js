857d569560addf929a6507e5424fd873f6f8b9f4
Brian Dukes <bdukes@etg-inc.net>
2008-12-04 20:32:18 +0000

    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Publish/EngagePublish;C1749

--- Scripts/ArticleSelector.js
+++ Scripts/ArticleSelector.js
@@ -1,13 +1,30 @@
 ﻿function PopulateArticlesList() {
     var categoryId = parseInt($get(CategoriesDropDownListId).value, 10);
-    Engage.Dnn.Publish.Services.PublishServices.GetArticlesByCategory(categoryId, GetArticlesSuccessFunction);
+    if (isFinite(categoryId)) {
+        Engage.Dnn.Publish.Services.PublishServices.GetArticlesByCategory(categoryId, GetArticlesSuccessFunction);
+    }
+    else {
+        clearDropDown($get(ArticlesDropDownListId));
+    }
 }
 
 function GetArticlesSuccessFunction(articlesList) {
     var articlesDropDown = $get(ArticlesDropDownListId);
 
-    articlesDropDown.options.length = 0;
+    clearDropDown(articlesDropDown);
     for (var i = 0; i < articlesList.length; ++i) {
         articlesDropDown.options[i] = new Option(articlesList[i].First, articlesList[i].Second);
+    }
+}
+
+function clearDropDown(dropDownList) {
+    /// <summary>
+    /// Clear the items from the drop down.
+    /// Based on AJAX Control Toolkit's CascadingDropDownList control's implementation.
+    /// </summary>
+    /// <returns />
+
+    while (0 < dropDownList.options.length) {
+        dropDownList.remove(0);
     }
 }
