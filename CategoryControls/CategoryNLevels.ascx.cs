e4d7caa2217f1e9e0e280450855a22e756a9f6a3
Brian Dukes <bdukes@etg-inc.net>
2010-06-18 17:57:09 +0000

    Cleaned up CategoryNLevels.ascx.cs
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Publish/EngagePublish;C10929

--- CategoryControls/CategoryNLevels.ascx.cs
+++ CategoryControls/CategoryNLevels.ascx.cs
@@ -1,13 +1,13 @@
-//Engage: Publish - http://www.engagesoftware.com
-//Copyright (c) 2004-2010
-//by Engage Software ( http://www.engagesoftware.com )
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-//DEALINGS IN THE SOFTWARE.
-
+// <copyright file="CategoryNLevels.ascx.cs" company="Engage Software">
+// Engage: Publish
+// Copyright (c) 2004-2010
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 namespace Engage.Dnn.Publish.CategoryControls
 {
@@ -16,156 +16,188 @@ namespace Engage.Dnn.Publish.CategoryControls
     using System.Web;
     using System.Web.UI;
     using System.Web.UI.WebControls;
+
     using DotNetNuke.Common.Utilities;
     using DotNetNuke.Entities.Modules;
     using DotNetNuke.Entities.Modules.Actions;
     using DotNetNuke.Security;
     using DotNetNuke.Services.Exceptions;
     using DotNetNuke.Services.Localization;
-    using Util;
 
+    using Engage.Dnn.Publish.Util;
+
+    using TreeNode = System.Windows.Forms.TreeNode;
+
+    /// <summary>
+    /// Displays items in a nested, unordered list
+    /// </summary>
     public partial class CategoryNLevels : ModuleBase, IActionable
     {
-        //private category id set from display loader
+        /// <summary>
+        /// The CSS Class added to highlight the current item
+        /// </summary>
         private const string HighlightCssClass = "NLevelCurrentItem";
 
-        private int nLevels = -1;
-        //private int mItems = -1;
-        private bool highlightCurrentItem;// = false;
+        /// <summary>
+        /// Whether to highlight the current item with the <see cref="HighlightCssClass"/>
+        /// </summary>
+        private bool highlightCurrentItem;
+
+        /// <summary>
+        /// The number of levels to display, or <c>-1</c> to display all levels
+        /// </summary>
+        private int numberOfLevels = -1;
+
+        /// <summary>
+        /// Whether to show the parent item at the top level or only show the children
+        /// </summary>
         private bool showParentItem = true;
 
-        #region Event Handlers
-
-        override protected void OnInit(EventArgs e)
+        /// <summary>
+        /// Gets the list of actions implemented by this module.
+        /// </summary>
+        /// <value>The module's actions.</value>
+        public ModuleActionCollection ModuleActions
         {
-            InitializeComponent();
+            get
+            {
+                return new ModuleActionCollection
+                    {
+                        {
+                            this.GetNextActionID(), 
+                            Localization.GetString("Settings", this.LocalResourceFile), 
+                            ModuleActionType.AddContent, 
+                            string.Empty, 
+                            string.Empty, 
+                            this.EditUrl("Settings"), 
+                            false, 
+                            SecurityAccessLevel.Edit, 
+                            true, 
+                            false
+                        }
+                    };
+            }
+        }
+
+        public int SetCategoryId { get; set; }
+
+        /// <remarks>
+        /// This method is exclusively used for cases where there is no "context" for the Category that a user has
+        /// linked to. If this is set to true, when the control loads both child categories and child articles are displayed. hk
+        /// </remarks>
+        public bool ShowAll { get; set; }
+
+        /// <summary>
+        /// Raises the <see cref="Control.Init"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> object that contains the event data.</param>
+        protected override void OnInit(EventArgs e)
+        {
+            this.Load += this.Page_Load;
             base.OnInit(e);
 
-            BindItemData();
-            SetupOptions();
-            SetPageTitle();
+            this.BindItemData();
+            this.SetupOptions();
+            this.SetPageTitle();
         }
 
-        private void InitializeComponent()
-        {
-            Load += Page_Load;
-        }
-
-        private void Page_Load(object sender, EventArgs e)
-        {
-            try
-            {
-                RecordView();
-                //N Levels M Items
-                BindData();
-            }
-            catch (Exception exc)
-            {
-                Exceptions.ProcessModuleLoadException(this, exc);
-            }
-        }
-        #endregion
-        
         private void BindData()
         {
-           //TODO: NLevels number of items is not currently used
+            // TODO: NLevels number of items is not currently used
 
-           //ToDO: figure out how to get the treeview to use the sorted datatable
-
-            string cacheKey = Utility.CacheKeyPublishCategoryNLevels + ItemId; // +"PageId";
-            var root = DataCache.GetCache(cacheKey) as System.Windows.Forms.TreeNode;
-            
+            // ToDO: figure out how to get the treeview to use the sorted datatable
+            string cacheKey = Utility.CacheKeyPublishCategoryNLevels + this.ItemId; // +"PageId";
+            var root = DataCache.GetCache(cacheKey) as TreeNode;
 
             if (root == null)
             {
-                root = ItemRelationship.GetAllChildrenNLevels(ItemId, nLevels, -1, PortalId);
-               if (root != null)
+                root = ItemRelationship.GetAllChildrenNLevels(this.ItemId, this.numberOfLevels, -1, this.PortalId);
+                if (root != null)
                 {
-                    DataCache.SetCache(cacheKey, root, DateTime.Now.AddMinutes(CacheTime));
-                    Utility.AddCacheKey(cacheKey, PortalId);
+                    DataCache.SetCache(cacheKey, root, DateTime.Now.AddMinutes(this.CacheTime));
+                    Utility.AddCacheKey(cacheKey, this.PortalId);
                 }
             }
 
-           //add the parent category to the list first.
-           if (showParentItem)
-           {
-                Category parentItem = Category.GetCategory(ItemId, PortalId);
-                #region showparent
+            // add the parent category to the list first.
+            if (this.showParentItem)
+            {
+                Category parentItem = Category.GetCategory(this.ItemId, this.PortalId);
+
                 if (parentItem != null)
                 {
-                    phNLevels.Controls.Add(new LiteralControl("<ul>"));
-                    phNLevels.Controls.Add(new LiteralControl("<li>"));
+                    this.phNLevels.Controls.Add(new LiteralControl("<ul>"));
+                    this.phNLevels.Controls.Add(new LiteralControl("<li>"));
 
-                    var hlParent = new HyperLink();
+                    var parentLink = new HyperLink();
 
                     if (parentItem.Disabled == false)
                     {
-                        hlParent.NavigateUrl = GetItemLinkUrl(parentItem.ItemId);
+                        parentLink.NavigateUrl = this.GetItemLinkUrl(parentItem.ItemId);
                     }
 
-                    hlParent.Text = parentItem.Name;
+                    parentLink.Text = parentItem.Name;
 
-                    if (highlightCurrentItem)
+                    if (this.highlightCurrentItem)
                     {
-                        int itemId;//=0;
-                        object o = Request.QueryString["ItemId"];
+                        int itemId; // =0;
+                        object o = this.Request.QueryString["ItemId"];
                         if (o != null && int.TryParse(o.ToString(), out itemId))
                         {
                             if (itemId == parentItem.ItemId)
                             {
-                                hlParent.CssClass = HighlightCssClass;
+                                parentLink.CssClass = HighlightCssClass;
                             }
                         }
                     }
-                    phNLevels.Controls.Add(hlParent);
-                    phNLevels.Controls.Add(new LiteralControl("</li>"));
 
-                    FillNLevelList(root, phNLevels);
-                    phNLevels.Controls.Add(new LiteralControl("</ul>"));
+                    this.phNLevels.Controls.Add(parentLink);
+                    this.phNLevels.Controls.Add(new LiteralControl("</li>"));
+
+                    this.FillNLevelList(root, this.phNLevels);
+                    this.phNLevels.Controls.Add(new LiteralControl("</ul>"));
                 }
                 else
                 {
-                    FillNLevelList(root, phNLevels);
+                    this.FillNLevelList(root, this.phNLevels);
                 }
-               #endregion
-
-           }
-           else
-           {
-               FillNLevelList(root, phNLevels);
-           }
-
-            
+            }
+            else
+            {
+                this.FillNLevelList(root, this.phNLevels);
+            }
         }
 
-
-        private void FillNLevelList(System.Windows.Forms.TreeNode root, PlaceHolder ph)
+        private void FillNLevelList(TreeNode root, PlaceHolder ph)
         {
-
             for (int i = 0; i < root.Nodes.Count; i++)
             {
-                System.Windows.Forms.TreeNode child = root.Nodes[i];
+                TreeNode child = root.Nodes[i];
                 if (child.Text.Length > 0)
                 {
                     var hl = new HyperLink();
                     int itemId = Convert.ToInt32(child.Tag, CultureInfo.InvariantCulture);
                     var it = Item.GetItemType(itemId);
 
-                    if (!Utility.IsDisabled(itemId, PortalId))
+                    if (!Utility.IsDisabled(itemId, this.PortalId))
                     {
                         Item curItem = BindItemData(itemId);
-                        hl.NavigateUrl = GetItemLinkUrl(itemId);
-                        if (curItem.NewWindow) hl.Target = "_blank";
+                        hl.NavigateUrl = this.GetItemLinkUrl(itemId);
+                        if (curItem.NewWindow)
+                        {
+                            hl.Target = "_blank";
+                        }
                     }
+
                     hl.Text = child.Text;
 
-                    ph.Controls.Add(new LiteralControl("<li class=\"" + it +"\">"));
+                    ph.Controls.Add(new LiteralControl("<li class=\"" + it + "\">"));
 
-                    //check if we should hightlight the current item, if so set the CSSClass
-                    if (highlightCurrentItem)
+                    // check if we should hightlight the current item, if so set the CSSClass
+                    if (this.highlightCurrentItem)
                     {
                         int querystringItemId;
-                        object o = Request.QueryString["ItemId"];
+                        object o = this.Request.QueryString["ItemId"];
                         if (o != null && int.TryParse(o.ToString(), out querystringItemId))
                         {
                             if (querystringItemId == itemId)
@@ -175,84 +207,45 @@ namespace Engage.Dnn.Publish.CategoryControls
                         }
                     }
 
-                   
                     ph.Controls.Add(hl);
 
                     if (child.Nodes.Count > 0)
                     {
                         ph.Controls.Add(new LiteralControl("<ul>"));
-                        FillNLevelList(child, ph);
+                        this.FillNLevelList(child, ph);
                         ph.Controls.Add(new LiteralControl("</ul>"));
                         child.Nodes.Clear();
                     }
-                
 
                     ph.Controls.Add(new LiteralControl("</li>"));
                 }
+
                 if (child.Nodes.Count > 0)
                 {
                     ph.Controls.Add(new LiteralControl("<ul class=\"EP_N_UL\">"));
-                    FillNLevelList(child, ph);
+                    this.FillNLevelList(child, ph);
                     ph.Controls.Add(new LiteralControl("</ul>"));
                 }
-                
-            }
-
-        }
-
-        #region Interface Members
-
-        public ModuleActionCollection ModuleActions
-        {
-            get
-            {
-                return new ModuleActionCollection
-                           {
-                                   {
-                                           GetNextActionID(),
-                                           Localization.GetString("Settings", LocalResourceFile),
-                                           ModuleActionType.AddContent, "", "", EditUrl("Settings"), false,
-                                           SecurityAccessLevel.Edit, true, false
-                                           }
-                           };
             }
         }
 
-        #endregion
-
-        //private int CategoryId
-        //{
-        //    get
-        //    {
-        //        string s = Request.QueryString["catid"];
-        //        return (s == null ? -1 : Convert.ToInt32(s));
-        //    }
-        //}
-
-        private void SetupOptions()
+        /// <summary>
+        /// Handles the <see cref="Control.Load"/> event of this control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void Page_Load(object sender, EventArgs e)
         {
-            object o = Settings["nLevels"];
-            if (o != null && !String.IsNullOrEmpty(o.ToString()))
+            try
             {
-                nLevels = Convert.ToInt32(o, CultureInfo.InvariantCulture);
+                this.RecordView();
+
+                // N Levels M Items
+                this.BindData();
             }
-
-            //object m = Settings["mItems"];
-            //if (m != null && !String.IsNullOrEmpty(m.ToString()))
-            //{
-            //    mItems = Convert.ToInt32(m, CultureInfo.InvariantCulture);
-            //}
-
-            o = Settings["HighlightCurrentItem"];
-            if (o != null && !String.IsNullOrEmpty(o.ToString()))
+            catch (Exception exc)
             {
-                highlightCurrentItem = Convert.ToBoolean(o.ToString(), CultureInfo.InvariantCulture);
-            }
-
-            o = Settings["ShowParentItem"];
-            if (o != null && !String.IsNullOrEmpty(o.ToString()))
-            {
-                showParentItem = Convert.ToBoolean(o.ToString(), CultureInfo.InvariantCulture);
+                Exceptions.ProcessModuleLoadException(this, exc);
             }
         }
 
@@ -261,39 +254,49 @@ namespace Engage.Dnn.Publish.CategoryControls
         /// </summary>
         private void RecordView()
         {
-            if (!VersionInfoObject.IsNew)
+            if (!this.VersionInfoObject.IsNew)
             {
                 string referrer = string.Empty;
                 if (HttpContext.Current.Request.UrlReferrer != null)
                 {
                     referrer = HttpContext.Current.Request.UrlReferrer.ToString();
                 }
+
                 string url = string.Empty;
                 if (HttpContext.Current.Request.RawUrl != null)
                 {
                     url = HttpContext.Current.Request.RawUrl;
                 }
 
-                
-                VersionInfoObject.AddView(UserId, TabId, HttpContext.Current.Request.UserHostAddress, HttpContext.Current.Request.UserAgent, referrer, url);
+                this.VersionInfoObject.AddView(
+                    this.UserId, this.TabId, HttpContext.Current.Request.UserHostAddress, HttpContext.Current.Request.UserAgent, referrer, url);
             }
         }
 
-        public int SetCategoryId
+        private void SetupOptions()
         {
-            get;
-            set;
-        }
+            object o = this.Settings["nLevels"];
+            if (o != null && !String.IsNullOrEmpty(o.ToString()))
+            {
+                this.numberOfLevels = Convert.ToInt32(o, CultureInfo.InvariantCulture);
+            }
 
-        /// <summary>
-        /// This method is exclusively used for cases where there is no "context" for the Category that a user has 
-        /// linked to. If this is set to true, when the control loads both child categories and child articles are displayed. hk
-        /// </summary>
-        public bool ShowAll
-        {
-            get;
-            set;
+            // object m = Settings["mItems"];
+            // if (m != null && !String.IsNullOrEmpty(m.ToString()))
+            // {
+            // mItems = Convert.ToInt32(m, CultureInfo.InvariantCulture);
+            // }
+            o = this.Settings["HighlightCurrentItem"];
+            if (o != null && !String.IsNullOrEmpty(o.ToString()))
+            {
+                this.highlightCurrentItem = Convert.ToBoolean(o.ToString(), CultureInfo.InvariantCulture);
+            }
+
+            o = this.Settings["ShowParentItem"];
+            if (o != null && !String.IsNullOrEmpty(o.ToString()))
+            {
+                this.showParentItem = Convert.ToBoolean(o.ToString(), CultureInfo.InvariantCulture);
+            }
         }
     }
 }
-
