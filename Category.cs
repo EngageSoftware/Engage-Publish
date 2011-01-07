4dd47fffdccf4260138f5d66fc193b4b04049b55
Brian Dukes <bdukes@etg-inc.net>
2011-01-07 14:54:55 +0000

    Throw proper exception when importing category without a valid Default Display Page
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Publish/EngagePublish;C13408

--- Category.cs
+++ Category.cs
@@ -767,7 +767,10 @@ namespace Engage.Dnn.Publish
                     // Default to setting for module
                     string settingName = Utility.PublishDefaultDisplayPage + this.PortalId.ToString(CultureInfo.InvariantCulture);
                     string setting = HostSettings.GetHostSetting(settingName);
-                    this._childDisplayTabId = Convert.ToInt32(setting, CultureInfo.InvariantCulture);
+                    if (!int.TryParse(setting, NumberStyles.Integer, CultureInfo.InvariantCulture, out this._childDisplayTabId))
+                    {
+                        throw new InvalidOperationException("Default Display Page setting must be set in order to import items");
+                    }
                 }
             }
 
