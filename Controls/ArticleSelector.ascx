f5c19bbd04ba1b2bf9504e24e13ffd7d84c9d550
Brian Dukes <bdukes@etg-inc.net>
2012-05-31 14:36:54 +0000

    Clean up ArticleSelector.js
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Publish/EngagePublish;C18234

--- Controls/ArticleSelector.ascx
+++ Controls/ArticleSelector.ascx
@@ -9,7 +9,7 @@
 <table cellspacing="0" cellpadding="0" border="0" class="SettingsTable articleOptionDropDown">
     <tr valign="top">
         <td class="SubHead"><dnn:Label ResourceKey="CategoriesLabel" runat="server" ControlName="CategoriesDropDownList" /></td>
-        <td><asp:DropDownList ID="CategoriesDropDownList" runat="server" CssClass="NormalTextBox" onchange="PopulateArticlesList()" /></td>
+        <td><asp:DropDownList ID="CategoriesDropDownList" runat="server" CssClass="NormalTextBox" onchange="Engage_Publish_ArticleSelector_PopulateArticlesList()" /></td>
     </tr>
     <tr>
         <td class="SubHead"><dnn:label resourcekey="lblArticleList" runat="server" ControlName="ArticlesDropDownList" /></td>
--- Scripts/ArticleSelector.js
+++ Scripts/ArticleSelector.js
@@ -1,30 +1,32 @@
-﻿function PopulateArticlesList() {
-    var categoryId = parseInt($get(CategoriesDropDownListId).value, 10);
-    if (isFinite(categoryId)) {
-        Engage.Dnn.Publish.Services.PublishServices.GetArticlesByCategory(categoryId, GetArticlesSuccessFunction);
-    }
-    else {
-        clearDropDown($get(ArticlesDropDownListId));
-    }
-}
+﻿/*globals Engage, $get, CategoriesDropDownListId, ArticlesDropDownListId */
+(function () {
+    'use strict';
+    var clearDropDown = function (dropDownList) {
+            /// <summary>
+            /// Clear the items from the drop down.
+            /// Based on AJAX Control Toolkit's CascadingDropDownList control's implementation.
+            /// </summary>
+            /// <returns />
 
-function GetArticlesSuccessFunction(articlesList) {
-    var articlesDropDown = $get(ArticlesDropDownListId);
+            while (0 < dropDownList.options.length) {
+                dropDownList.remove(0);
+            }
+        };
+    
+    window.Engage_Publish_ArticleSelector_PopulateArticlesList = function () {
+        var categoryId = parseInt($get(CategoriesDropDownListId).value, 10);
+        if (isFinite(categoryId)) {
+            Engage.Dnn.Publish.Services.PublishServices.GetArticlesByCategory(categoryId, function (articlesList) {
+                var articlesDropDown = $get(ArticlesDropDownListId),
+                    i = 0;
 
-    clearDropDown(articlesDropDown);
-    for (var i = 0; i < articlesList.length; ++i) {
-        articlesDropDown.options[i] = new Option(articlesList[i].First, articlesList[i].Second);
-    }
-}
-
-function clearDropDown(dropDownList) {
-    /// <summary>
-    /// Clear the items from the drop down.
-    /// Based on AJAX Control Toolkit's CascadingDropDownList control's implementation.
-    /// </summary>
-    /// <returns />
-
-    while (0 < dropDownList.options.length) {
-        dropDownList.remove(0);
-    }
-}
+                clearDropDown(articlesDropDown);
+                for (; i < articlesList.length; i += 1) {
+                    articlesDropDown.options[i] = new Option(articlesList[i].First, articlesList[i].Second);
+                }
+            });
+        } else {
+            clearDropDown($get(ArticlesDropDownListId));
+        }
+    };
+}());
