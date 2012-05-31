d8412d059599486c70428741e3d66cc20fac9f1d
Brian Dukes <bdukes@etg-inc.net>
2012-05-31 14:50:10 +0000

    Clean up CategoryFeature.ascx.cs
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Publish/EngagePublish;C18235

--- CategoryControls/CategoryFeature.ascx.cs
+++ CategoryControls/CategoryFeature.ascx.cs
@@ -1,27 +1,27 @@
-//Engage: Publish - http://www.engagesoftware.com
-//Copyright (c) 2004-2011
-//by Engage Software ( http://www.engagesoftware.com )
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-//DEALINGS IN THE SOFTWARE.
+// <copyright file="CategoryFeature.ascx.cs" company="Engage Software">
+// Engage: Publish
+// Copyright (c) 2004-2012
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 namespace Engage.Dnn.Publish.CategoryControls
 {
     using System;
     using System.Data;
+    using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
     using System.Text;
     using System.Web.UI.WebControls;
 
-    using DotNetNuke.Common.Utilities;
     using DotNetNuke.Entities.Modules;
     using DotNetNuke.Entities.Modules.Actions;
     using DotNetNuke.Security;
     using DotNetNuke.Services.Exceptions;
-    using DotNetNuke.Services.Localization;
 
     using Engage.Dnn.Publish.Util;
 
@@ -34,9 +34,17 @@ namespace Engage.Dnn.Publish.CategoryControls
                 return new ModuleActionCollection
                     {
                         {
-                            this.GetNextActionID(), Localization.GetString("Settings", this.LocalResourceFile), ModuleActionType.AddContent, 
-                            string.Empty, string.Empty, this.EditUrl("Settings"), false, SecurityAccessLevel.Edit, true, false
-                            }
+                            this.GetNextActionID(),
+                            this.Localize("Settings"),
+                            ModuleActionType.AddContent,
+                            string.Empty,
+                            string.Empty,
+                            this.EditUrl("Settings"),
+                            false,
+                            SecurityAccessLevel.Edit,
+                            true,
+                            false,
+                        },
                     };
             }
         }
@@ -62,7 +70,7 @@ namespace Engage.Dnn.Publish.CategoryControls
             get
             {
                 object o = this.Settings["cfEnableRss"];
-                return o == null ? false : Convert.ToBoolean(o, CultureInfo.InvariantCulture);
+                return o != null && Convert.ToBoolean(o, CultureInfo.InvariantCulture);
             }
         }
 
@@ -71,7 +79,7 @@ namespace Engage.Dnn.Publish.CategoryControls
             get
             {
                 object o = this.Settings["cfRandomize"];
-                return o == null ? false : Convert.ToBoolean(o, CultureInfo.InvariantCulture);
+                return o != null && Convert.ToBoolean(o, CultureInfo.InvariantCulture);
             }
         }
 
@@ -85,14 +93,9 @@ namespace Engage.Dnn.Publish.CategoryControls
             this.Load += this.Page_Load;
             base.OnInit(e);
 
-            if (ItemId == -1)
-            {
-                this.VersionInfoObject = Category.Create(PortalId);
-            }
-            else
-            {
-                this.VersionInfoObject = Category.GetCategory(ItemId, PortalId);
-            }
+            this.VersionInfoObject = this.ItemId == -1
+                                         ? Category.Create(this.PortalId)
+                                         : Category.GetCategory(this.ItemId, this.PortalId);
 
             if (ItemId > 0 && (Item.GetItemType(ItemId, PortalId) == "Category" || Item.GetItemType(ItemId, PortalId) == "TopLevelCategory"))
             {
@@ -100,54 +103,53 @@ namespace Engage.Dnn.Publish.CategoryControls
             }
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1706:ShortAcronymsShouldBeUppercase", MessageId = "Member", 
-            Justification = "Controls use lower case prefix (Not an acronym)")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Member", 
-            Justification = "Controls use lower case prefix")]
+        [SuppressMessage("Microsoft.Naming", "CA1706:ShortAcronymsShouldBeUppercase", MessageId = "Member", Justification = "Controls use lower case prefix (Not an acronym)")]
+        [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Member", Justification = "Controls use lower case prefix")]
         protected void dlItems_ItemDataBound(object sender, DataListItemEventArgs e)
         {
-            if (e != null)
+            if (e == null)
             {
-                Panel pnlThumbnail = (Panel)e.Item.FindControl("pnlThumbnail");
-                Panel pnlReadMore = (Panel)e.Item.FindControl("pnlReadMore");
-                Literal lblDescription = (Literal)e.Item.FindControl("lblDescription");
-                HyperLink lnkName = (HyperLink)e.Item.FindControl("lnkName");
-                HyperLink imgThumbnail = (HyperLink)e.Item.FindControl("imgThumbnail");
-
-                if (pnlThumbnail != null)
-                {
-                    pnlThumbnail.Visible = DisplayOption == ArticleViewOption.TitleAndThumbnail || DisplayOption == ArticleViewOption.Thumbnail;
-                }
-
-                if (lblDescription != null)
-                {
-                    lblDescription.Visible = DisplayOption == ArticleViewOption.Abstract || DisplayOption == ArticleViewOption.Thumbnail;
-                }
-
-                if (pnlReadMore != null)
-                {
-                    pnlReadMore.Visible = DisplayOption == ArticleViewOption.Abstract || DisplayOption == ArticleViewOption.Thumbnail;
-                }
-
-                ////This code makes sure that an article that is disabled does not get a link.
-                if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
-                {
-                    DataRow dr = ((DataRowView)e.Item.DataItem).Row;
-                    int linkedItemId = (int)dr["ItemId"];
-
-                    if (Engage.Dnn.Publish.Util.Utility.IsDisabled(linkedItemId, PortalId))
-                    {
-                        lnkName.NavigateUrl = string.Empty;
-                        imgThumbnail.NavigateUrl = string.Empty;
-                        pnlReadMore.Visible = false;
-                    }
-
-                    // if (pnlThumbnail != null && (dr["Thumbnail"] == null || !Utility.HasValue(dr["Thumbnail"].ToString())))
-                    // {
-                    // pnlThumbnail.CssClass += " item_listing_nothumbnail";
-                    // }
-                }
+                return;
             }
+
+            var pnlThumbnail = (Panel)e.Item.FindControl("pnlThumbnail");
+            var pnlReadMore = (Panel)e.Item.FindControl("pnlReadMore");
+            var lblDescription = (Literal)e.Item.FindControl("lblDescription");
+            var lnkName = (HyperLink)e.Item.FindControl("lnkName");
+            var imgThumbnail = (HyperLink)e.Item.FindControl("imgThumbnail");
+
+            if (pnlThumbnail != null)
+            {
+                pnlThumbnail.Visible = this.DisplayOption == ArticleViewOption.TitleAndThumbnail || this.DisplayOption == ArticleViewOption.Thumbnail;
+            }
+
+            if (lblDescription != null)
+            {
+                lblDescription.Visible = this.DisplayOption == ArticleViewOption.Abstract || this.DisplayOption == ArticleViewOption.Thumbnail;
+            }
+
+            if (pnlReadMore != null)
+            {
+                pnlReadMore.Visible = this.DisplayOption == ArticleViewOption.Abstract || this.DisplayOption == ArticleViewOption.Thumbnail;
+            }
+
+            if (e.Item.ItemType != ListItemType.Item && e.Item.ItemType != ListItemType.AlternatingItem)
+            {
+                return;
+            }
+
+            // This code makes sure that an article that is disabled does not get a link.
+            var dr = ((DataRowView)e.Item.DataItem).Row;
+            var linkedItemId = (int)dr["ItemId"];
+
+            if (!Utility.IsDisabled(linkedItemId, this.PortalId))
+            {
+                return;
+            }
+
+            lnkName.NavigateUrl = string.Empty;
+            imgThumbnail.NavigateUrl = string.Empty;
+            pnlReadMore.Visible = false;
         }
 
         private void BindData()
@@ -155,59 +157,46 @@ namespace Engage.Dnn.Publish.CategoryControls
             if (!this.VersionInfoObject.IsNew)
             {
                 // get relationship type based on selection
-                int relationshipTypeId = RelationshipType.ItemToFeaturedItem.GetId();
+                var relationshipTypeId = RelationshipType.ItemToFeaturedItem.GetId();
 
                 if (this.EnableRss)
                 {
                     this.lnkRss.Visible = true;
 
-                    string rssImage = Localization.GetString("rssImage", this.LocalResourceFile);
+                    string rssImage = this.Localize("rssImage");
 #if DEBUG
                     rssImage = rssImage.Replace("[L]", string.Empty);
 #endif
 
                     this.lnkRss.ImageUrl = ApplicationUrl + rssImage; // "/images/xml.gif";
-                    this.lnkRss.Attributes.Add("alt", Localization.GetString("rssAlt", this.LocalResourceFile));
-                    var rssUrl = new StringBuilder();
-                    rssUrl.Append(ApplicationUrl);
-                    rssUrl.Append(DesktopModuleFolderName);
-                    rssUrl.Append("epRss.aspx?");
-                    rssUrl.AppendFormat("ItemId={0}", this.VersionInfoObject.ItemId);
-                    rssUrl.AppendFormat("&RelationshipTypeId={0}&PortalId={1}&DisplayType=CategoryFeature", relationshipTypeId, this.PortalId);
-                    this.lnkRss.NavigateUrl = rssUrl.ToString();
-                    this.SetRssUrl(this.lnkRss.NavigateUrl, Localization.GetString("rssText", this.LocalResourceFile));
+                    this.lnkRss.Attributes.Add("alt", this.Localize("rssAlt"));
+                    this.lnkRss.NavigateUrl = new StringBuilder(ApplicationUrl)
+                        .Append(DesktopModuleFolderName)
+                        .Append("epRss.aspx?")
+                        .AppendFormat("ItemId={0}", this.VersionInfoObject.ItemId)
+                        .AppendFormat("&RelationshipTypeId={0}&PortalId={1}&DisplayType=CategoryFeature", relationshipTypeId, this.PortalId)
+                        .ToString();
+                    this.SetRssUrl(this.lnkRss.NavigateUrl, this.Localize("rssText"));
                 }
 
                 // get initial data set
-                string cacheKey = Utility.CacheKeyPublishCategoryFeature + this.VersionInfoObject.ItemId.ToString(CultureInfo.InvariantCulture);
+                var cacheKey = Utility.CacheKeyPublishCategoryFeature + this.VersionInfoObject.ItemId.ToString(CultureInfo.InvariantCulture);
                     
                     // +"PageId";
-                var dv = DataCache.GetCache(cacheKey) as DataView;
-
-                if (dv == null)
-                {
-                    DataSet dsp = Item.GetParentItems(this.VersionInfoObject.ItemId, this.PortalId, relationshipTypeId);
-                    dv = dsp.Tables[0].DefaultView;
-                    if (dv != null)
-                    {
-                        DataCache.SetCache(cacheKey, dv, DateTime.Now.AddMinutes(this.CacheTime));
-                        Utility.AddCacheKey(cacheKey, this.PortalId);
-                    }
-                }
-
+                var dv = Utility.GetValueFromCache(this.PortalId, cacheKey, () => 
+                    Item.GetParentItems(this.VersionInfoObject.ItemId, this.PortalId, relationshipTypeId).Tables[0].DefaultView);
                 this.dlItems.DataSource = this.RandomArticle ? Utility.GetRandomItem(dv) : dv;
                 this.dlItems.DataBind();
             }
 
-            if (this.VersionInfoObject.IsNew && this.IsAdmin)
+            if (!this.VersionInfoObject.IsNew || !this.IsAdmin)
             {
-                // based on the user display a message (admin only))
-                this.lblNoData.Text = Localization.GetString("NoApprovedVersion", this.LocalResourceFile);
-                this.lblNoData.Visible = true;
-
-                // this.dlCategories.Visible = false;
-                // this.dlItems.Visible = false;
+                return;
             }
+
+            // based on the user display a message (admin only))
+            this.lblNoData.Text = this.Localize("NoApprovedVersion");
+            this.lblNoData.Visible = true;
         }
 
         private void Page_Load(object sender, EventArgs e)
